83bc9c680632826a848c77fb042c78f5587840db
popsbear1 <bigoreuel.rb@gmail.com>
2017-03-17 14:30:48 +0800

    yhe

--- Script/javaScript.js
+++ Script/javaScript.js
@@ -1,2 +1,12 @@
 <script>
+	var assname;
+	var subject;
+	var details;
+	function my_function () {
+		assname = document.getElementById("aname").value;
+		subject = document.getElementById("asubj").value;
+		details = document.getElementById('adet').value;
+		alert("assname");
+	}
+	
 </script>
