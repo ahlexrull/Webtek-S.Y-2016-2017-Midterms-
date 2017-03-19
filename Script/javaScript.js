4c7b43b5616dbe5a3f54988314e730f4fe60c2b7
Reuel M. Bigo <bigoreuel.rb@gmail.com>
2017-03-19 21:20:38 +0800

    assignment saving in localstorage.. unfinished

--- Script/javaScript.js
+++ Script/javaScript.js
@@ -1,12 +1,23 @@
-<script>
-	var assname;
-	var subject;
-	var details;
-	function my_function () {
-		assname = document.getElementById("aname").value;
-		subject = document.getElementById("asubj").value;
-		details = document.getElementById('adet').value;
-		alert("assname");
-	}
-	
-</script>
+
+function get_AssgDetails () {
+
+	var newAssgDetails = {};
+
+	newAssgDetails	.assgName = document.getElementById('aname').value;
+	newAssgDetails	.assgSubject = document.getElementById('asubj').value;
+	newAssgDetails	.assgDetails = document.getElementById('adate').value;
+	newAssgDetails	.assgDate = document.getElementById('adate').value;
+	//alert(newAssgDetails.assgName);
+
+	if (localStorage.assignment) {
+		assignment = JSON.parse(localStorage.getItem('assignment'));
+	}else{
+		assignment = [];
+	}s
+
+	assignment.push(newAssgDetails);
+	localStorage.setItem('assignment', JSON.stringify(assignment));
+
+	var robject = localStorage.getItem('assignment');
+	console.log('robject', JSON.parse(robject));
+}
