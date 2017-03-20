b889ab1d413f4712c9adcd9407f49b47f2866639
Benedict Suarez <benedictsuarez888@gmail.com>
2017-03-20 21:48:43 +0800

    Updated.

--- Script/javaScript.js
+++ Script/javaScript.js
@@ -1,12 +1,37 @@
-<script>
-	var assname;
-	var subject;
-	var details;
-	function my_function () {
-		assname = document.getElementById("aname").value;
-		subject = document.getElementById("asubj").value;
-		details = document.getElementById('adet').value;
-		alert("assname");
+
+function get_AssgDetails () {
+
+	var newAssgDetails = {};
+
+	newAssgDetails	.assgName = document.getElementById('aname').value;
+	newAssgDetails	.assgSubject = document.getElementById('asubj').value;
+	newAssgDetails	.assgDetails = document.getElementById('adate').value;
+	newAssgDetails	.assgDate = document.getElementById('adate').value;
+
+	if (localStorage.assignment) {
+		assignment = JSON.parse(localStorage.getItem('assignment'));
+	}else{
+		assignment = [];
 	}
-	
-</script>
+
+	assignment.push(newAssgDetails);
+	localStorage.setItem('assignment', JSON.stringify(assignment));
+
+	var assignmentDetails = JSON.parse(localStorage.getItem('assignment'));
+
+	for (var i=0, len = assignmentDetails.length;i < len;i++ ){
+		console.log(assignmentDetails[i]);
+	}
+}
+
+function display_AssgDetails (){
+	if (localStorage.assignment){
+		var assignmentDetails = JSON.parse(localStorage.getItem('assignment'));
+
+		for (var i=0, len = assignmentDetails.length;i < len;i++ ){
+			console.log(assignmentDetails[i]);
+		}
+	}else{
+		alert("No assignments as of the moment.");
+	}
+}
