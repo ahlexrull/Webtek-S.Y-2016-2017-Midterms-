534725de21808566c80dec581d028a75d877c928
Benedict Suarez <benedictsuarez888@gmail.com>
2017-03-20 21:55:34 +0800

    Updated JS with Local Storage ni Kuya Reuel.

--- Script/javaScript.js
+++ Script/javaScript.js
@@ -7,10 +7,6 @@ function get_AssgDetails () {
 	newAssgDetails	.assgSubject = document.getElementById('asubj').value;
 	newAssgDetails	.assgDetails = document.getElementById('adate').value;
 	newAssgDetails	.assgDate = document.getElementById('adate').value;
-<<<<<<< HEAD
-=======
-	//alert(newAssgDetails.assgName);
->>>>>>> c696e2db57d31c37bf309d90cebf4c57f6cb689b
 
 	if (localStorage.assignment) {
 		assignment = JSON.parse(localStorage.getItem('assignment'));
@@ -21,7 +17,6 @@ function get_AssgDetails () {
 	assignment.push(newAssgDetails);
 	localStorage.setItem('assignment', JSON.stringify(assignment));
 
-<<<<<<< HEAD
 	var assignmentDetails = JSON.parse(localStorage.getItem('assignment'));
 
 	for (var i=0, len = assignmentDetails.length;i < len;i++ ){
@@ -38,11 +33,5 @@ function display_AssgDetails (){
 		}
 	}else{
 		alert("No assignments as of the moment.");
-=======
-	var robject = JSON.parse(localStorage.getItem('assignment'));
-
-	for (var i=0, len = localStorage.length;i < len;i++ ){
-		console.log(localStorage.getItem(localStorage.key(i)));
->>>>>>> c696e2db57d31c37bf309d90cebf4c57f6cb689b
 	}
 }
