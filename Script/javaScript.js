8ab067cdefe59a7755f504745fb136c17ccf6d1e
denba-desu <2152598@slu.edu.ph>
2017-03-21 21:14:32 +0800

    Updated script

--- Script/javaScript.js
+++ Script/javaScript.js
@@ -17,21 +17,46 @@ function get_AssgDetails () {
 	assignment.push(newAssgDetails);
 	localStorage.setItem('assignment', JSON.stringify(assignment));
 
-	var assignmentDetails = JSON.parse(localStorage.getItem('assignment'));
-
-	for (var i=0, len = assignmentDetails.length;i < len;i++ ){
-		console.log(assignmentDetails[i]);
 	}
+
+function get_CsDetails () {
+
+	var newCsgDetails = {};
+
+	newCsDetails	.csName = document.getElementById('csname').value;
+	newCsDetails	.csScore = document.getElementById('csscore').value;
+	newCsDetails	.csPoint = document.getElementById('cspoint').value;
+
+	if (localStorage.classStanding) {
+		classStanding = JSON.parse(localStorage.getItem('classStanding'));
+	}else{
+		classStanding = [];
+	}             
+
+	classStanding.push(newCsgDetails);
+	localStorage.setItem('classStanding', JSON.stringify(classStanding));
+
 }
 
 function display_AssgDetails (){
 	if (localStorage.assignment){
 		var assignmentDetails = JSON.parse(localStorage.getItem('assignment'));
-
+		alert("Hello");
 		for (var i=0, len = assignmentDetails.length;i < len;i++ ){
 			console.log(assignmentDetails[i]);
 		}
 	}else{
 		alert("No assignments as of the moment.");
+	}
+}
+function display_CsDetails (){
+	if (localStorage.classStanding){
+		var CsDetails = JSON.parse(localStorage.getItem('classStanding'));
+
+		for (var i=0, len = CsDetails.length;i < len;i++ ){
+			console.log(CsDetails[i]);
+		}
+	}else{
+		alert("No Class Standing as of the moment.");
 	}
 }
