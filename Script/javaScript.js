75a06de02ce93de3ea390444dfdc42042c0e6fc3
Benedict Suarez <benedictsuarez888@gmail.com>
2017-03-24 23:42:09 +0800

    Update the table in the viewassign.html and edit also the display_AssgDetails function in the javaScript.js.

--- Script/javaScript.js
+++ Script/javaScript.js
@@ -85,7 +85,7 @@ function display_AssgDetails (){
 		for (var i=0, len = assignmentDetails.length;i < len;i++ ){
 			console.log(assignmentDetails[i]);
 			var table = document.getElementById("assignmentTable");
-			var row = table.insertRow(0);
+			var row = table.insertRow(1);
 			var cell1 = row.insertCell(0);
 			var cell2 = row.insertCell(1);
 			var cell3 = row.insertCell(2);
