a048eb1c1259683ce04e925f81022f987497a772
Benedict Suarez <benedictsuarez888@gmail.com>
2017-03-29 07:24:13 +0800

    Added countdown but under revisions.

--- Script/javaScript.js
+++ Script/javaScript.js
@@ -126,6 +126,41 @@ function display_AssgDetails (){
 				//duedate.setFullYear(newdate[0], newdate[]);
 				//cell6.innerHTML = due_countdown(assignmentDetails[i].assgDate);
 			}
+
+			{
+				// Set the date we're counting down to
+				var countDownDate = new Date(assignmentDetails[i].assgDate).getTime();
+
+				// Update the count down every 1 second
+				var x = setInterval(function() {
+
+					// Get todays date and time
+					var now = new Date().getTime();
+
+					// Find the distance between now an the count down date
+					var distance = countDownDate - now;
+
+				 	// Time calculations for days, hours, minutes and seconds
+					var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+					var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+					var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+					var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+					// Display the result in the element with id="assignmentTable"
+					var ctdown = document.getElementById("assignmentTable").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+					
+					cell6.innerHTML = ctdown;
+
+					// If the count down is finished, write some text 
+					if (distance < 0) {
+						clearInterval(x);
+						var ctdown = document.getElementById("assignmentTable").innerHTML = "EXPIRED";
+
+						cell6.innerHTML = ctdown;
+						
+				  	}
+				}, 1000);
+			}
 			
 		}
 
@@ -218,3 +253,4 @@ function remove_ALLCs (){
 		alert("NO CS records to delete");
 	}
 }
+
